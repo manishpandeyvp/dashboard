30cd56f8fafdd95e8455393da4352a3fd28b601e
Manish Pandey <2018140@iiitdmj.ac.in>
2020-05-26 20:37:45 +0530

    Fifth Commit

---
+++ lib/faculty_list.dart
@@ -0,0 +1,106 @@
+import 'search_faculty_card.dart';
+import 'package:flutter/material.dart';
+import 'faculty_card.dart';
+
+var facultyList = {
+  'Vijay Kumar Gupta': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Vijay Kumar Gupta',
+    desc: 'Assistant Professor',
+  ),
+  'Ravi Panwar': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Ravi Panwar',
+    desc: 'Assistant Professor',
+  ),
+  'Atul Kumar': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Atul Kumar',
+    desc: 'Assistant Professor',
+  ),
+  'Himanshu Shekhar Nanda': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Himanshu Shekhar Nanda',
+    desc: 'Assistant Professor',
+  ),
+  'Mamta Anand': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Mamta Anand',
+    desc: 'Assistant Professor',
+  ),
+  'Saurabh Pratap Singh': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Saurabh Pratap Singh',
+    desc: 'Assistant Professor',
+  ),
+  'Ayan Seal': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Ayan Seal',
+    desc: 'Assistant Professor',
+  ),
+  'Assistant Professor1': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Vijay Kumar Gupta',
+    desc: 'Assistant Professor',
+  ),
+  'Assistant Professor2': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Ravi Panwar',
+    desc: 'Assistant Professor',
+  ),
+  'Assistant Professor3': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Atul Kumar',
+    desc: 'Assistant Professor',
+  ),
+  'Assistant Professor4': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Himanshu Shekhar Nanda',
+    desc: 'Assistant Professor',
+  ),
+  'Assistant Professor5': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Mamta Anand',
+    desc: 'Assistant Professor',
+  ),
+  'Assistant Professor6': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Saurabh Pratap Singh',
+    desc: 'Assistant Professor',
+  ),
+  'Assistant Professor7': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Ayan Seal',
+    desc: 'Assistant Professor',
+  ),
+  'Mess Committee1': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Ayan Seal',
+    desc: 'Mess Committee',
+  ),
+  'Mess Committee2': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Trivesh Kumar',
+    desc: 'Mess Committee',
+  ),
+  'Dean Students': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Prashant Kumar Jain',
+    desc: 'Dean Students',
+  ),
+  'Dean Academics': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Vijay Kumar Gupta',
+    desc: 'Dean Academics',
+  ),
+  'Mess Committee3': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Mamta Anand',
+    desc: 'Mess Committee',
+  ),
+  'Director': SearchFacultyCard(
+    icon: 'images/download.png',
+    text: 'Sanjeev Jain',
+    desc: 'Director',
+  ),
+};
---
+++ lib/search_faculty_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'all_constants.dart';
+import 'faculty_card.dart';
+
+class SearchFacultyCard extends StatefulWidget {
+  SearchFacultyCard({
+    @required this.text,
+    @required this.icon,
+    @required this.desc,
+  });
+  final String text;
+  final String icon;
+  final String desc;
+
+  @override
+  _SearchFacultyCardState createState() => _SearchFacultyCardState();
+}
+
+class _SearchFacultyCardState extends State<SearchFacultyCard> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => FacultyCard()));
+        });
+      },
+      child: Container(
+        height: 60.0,
+        decoration: BoxDecoration(
+          color: kBackgroundColor,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: CircleAvatar(
+                radius: 25.0,
+                backgroundImage: AssetImage(widget.icon),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 28.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      widget.text,
+                      style: TextStyle(fontSize: 17.0, color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 2.0,
+                    ),
+                    Text(
+                      widget.desc,
+                      style: TextStyle(fontSize: 13.0, color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
